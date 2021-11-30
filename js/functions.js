cbb56a9d23d1c7652243e0c765821abae003c79e
Jaren Borst <jarenborst@student.rocva.nl>
2021-11-30 10:26:27 +0100

    challenge

---
+++ js/functions.js
@@ -0,0 +1,23 @@
+function picture(){ 
+    document.getElementById('pic').style.display='block';
+}
+function code() {
+    document.getElementById('code').style.display='block';
+}
+function setColor(element, kleur){ 
+
+    element.style.color = kleur; 
+
+}
+function hideMe(){
+document.getElementById("blok").style.display = "none"; 
+document.getElementById("show").style.display = "block"; 
+}
+function show(){
+    document.getElementById("blok").style.display = "block";
+    document.getElementById("show").style.display = "none";
+}
+
+function pa() {
+    document.getElementById('blok').style.display='block';
+}
